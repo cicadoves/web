49db65ece815459a5f383f632fe9a4c48ecad3c5
cicadoves <o7infiltrate@gmail.com>
2021-05-27 22:19:01 +0800

    fix

--- calc.js
+++ calc.js
@@ -1,9 +1,11 @@
-var x = document.getElementById('first');
-var y = document.getElementById('second');
-var z = document.getElementById('result')
+var inp1 = document.getElementById('first');
+var inp2 = document.getElementById('second');
+var out = document.getElementById('result');
 
 calc.addEventListener('submit', function(event){
-  event.preventDefault()
-  sum = parseFloat(x.value) + parseFloat(y.value)
-  z.innerText = sum
-})
+  val1 = parseFloat(inp1.value);
+  val2 = parseFloat(inp2.value);
+  sum = val1 + val2;
+  out.innerText = sum;
+  event.preventDefault();
+});
