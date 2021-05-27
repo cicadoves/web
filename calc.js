c95cd0f128da45b9d7b8416e2664107f9382c42b
cicadoves <o7infiltrate@gmail.com>
2021-05-27 23:25:50 +0800

    js

--- calc.js
+++ calc.js
@@ -3,9 +3,9 @@ var inp2 = document.getElementById('second');
 var out = document.getElementById('result');
 
 calc.addEventListener('submit', function(event){
+  event.preventDefault();
   val1 = parseFloat(inp1.value);
   val2 = parseFloat(inp2.value);
   sum = val1 + val2;
   out.innerText = sum;
-  event.preventDefault();
 });
