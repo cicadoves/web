040e1f9c34aeb2428dd26faf83034938d9b46745
cicadoves <cicadoves@gmail.com>
2021-06-07 14:42:10 +0800

    omniforcess

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function check() {
 }
 
 function col(text) {
-	for (const i of Array(table.length).keys()) {
+	for (const i of Array(table.length+1).keys()) {
 		table.rows[i].cells[0].innerHTML = text;
 	}
 }
