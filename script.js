4714cbb30f0b84d832375ca79789ec4b491205af
cicadoves <cicadoves@gmail.com>
2021-06-07 16:33:21 +0800

    expand

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-var box = document.getElementById('box');
+var box1 = document.getElementById('box1');
+var box2 = document.getElementById('box2');
 var table = document.getElementById('table');
 
 function check() {
-	if (box.checked) {
-		col('-1');
+	if (box1.checked) {
+		col('check', 0);
 	} else {
-		col('-2');
+		col('', 0);
+	}
+
+	if (box2.checked) {
+		col('check', 1);
+	} else {
+		col('', 1);
 	}
 }
 
-function col(text) {
+function col(text, col) {
 	for (var i = 0; i < table.rows.length; i++) {
-		table.rows[i].cells[0].innerHTML = text;
+		table.rows[i].cells[col].innerHTML = text;
 	}
 }
