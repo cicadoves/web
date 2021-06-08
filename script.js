0f593c6f807f3f8d3f5f0beacfa7ce1c54041209
cicadoves <cicadoves@gmail.com>
2021-06-08 13:39:13 +0800

    bugfix

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ var box = document.getElementById('box');
 var table = document.getElementById('table');
 
 function check() {
-	if (box1.checked) {
-		writecol('1');
+	if (box.checked) {
+		column('1');
 	} else {
-		writecol('0');
+		column('0');
 	}
 }
 
-function writecol(text) {
+function column(text) {
 	for (var i = 0; i < table.rows.length; i++) {
 		table.rows[i].cells[0].innerHTML = text;
 	}
