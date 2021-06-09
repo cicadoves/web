5642e27496944db151fb83aaea8cacef8b35307f
cicadoves <cicadoves@insomnia247.nl>
2021-06-09 11:29:58 +0200

    teaching js

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 var box = document.getElementById('box');
+var box2 = document.getElementById('box2');
 var table = document.getElementById('table');
 
 function check() {
 	if (box.checked) {
-		args = ['oniichan', 'yamette', 'and im like yo wtf this is just a string'];
+		args = ['a', 'b', 'c'];
 	} else {
-		args = ['oneechan', 'iyaa', 'and he is like no no no if else else if'];
+		args = ['x', 'y', 'z'];
 	}
 	column(args)
+
+	if (box2.checked) {
+		args = ['d', 'e', 'f'];
+	} else {
+		args = ['u', 'v', 'w'];
+	}
+	column2(args)
 }
 
 function column(args) {
 	for (var i = 0; i < table.rows.length; i++) {
 		table.rows[i].cells[0].innerHTML = args[i];
+	}
+}
+
+function column2(args) {
+	for (var i = 0; i < table.rows.length; i++) {
+		table.rows[i].cells[1].innerHTML = args[i];
 	}
 }
