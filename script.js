a86bb633594faa22b6d8530d3ad889d58f07f27f
cicadoves <cicadoves@gmail.com>
2021-06-13 12:16:26 +0200

    yay, more bugs

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 var box = document.getElementById('box');
-//var box2 = document.getElementById('box2');
 var res = document.getElementById('res');
 var table = document.getElementById('table');
 
 function check() {
 	if (box.checked) {
 		x = '-';
-//		args = ['a', 'b', 'c'];
 	} else {
 		x = '+';
-//		args = ['x', 'y', 'z'];
 	}
-//	column(args)
+	table.rows[0].cells[1].innerHTML = x;
 
-//	if (box2.checked) {
+//	if (box.checked) {
 //		args = ['d', 'e', 'f'];
 //	} else {
 //		args = ['u', 'v', 'w'];
 //	}
-//	column2(args)
-	setsym(x)
+//	column(args);
 }
 
 function calc() {
-	a = table.rows[0].cells[0].innerHTML;
-	b = table.rows[0].cells[2].innerHTML;
+	a = parseFloat(table.rows[0].cells[0].text);
+	b = parseFloat(table.rows[0].cells[2].text);
 	if (box.checked) {
 		result = a - b;
 	} else {
@@ -39,12 +35,3 @@ function calc() {
 //	}
 //}
 
-//function column2(args) {
-//	for (var i = 0; i < table.rows.length; i++) {
-//		table.rows[i].cells[1].innerHTML = args[i];
-//	}
-//}
-
-function setsym(x) {
-	table.rows[0].cells[1].innerHTML = x;
-}
