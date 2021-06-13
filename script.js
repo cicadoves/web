834dc4b2f8c69afb26f8a662f5857aea2ff5dfb9
cicadoves <cicadoves@gmail.com>
2021-06-13 11:46:50 +0200

    cleanup

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 var box = document.getElementById('box');
-var box2 = document.getElementById('box2');
+//var box2 = document.getElementById('box2');
 var table = document.getElementById('table');
 
 function check() {
 	if (box.checked) {
-		x = '+';
+		x = '-';
 //		args = ['a', 'b', 'c'];
-//	} else {
+	} else {
+		x = '+';
 //		args = ['x', 'y', 'z'];
 	}
 //	column(args)
 
-	if (box2.checked) {
-		x = 'x';
+//	if (box2.checked) {
 //		args = ['d', 'e', 'f'];
 //	} else {
 //		args = ['u', 'v', 'w'];
@@ -22,25 +22,23 @@ function check() {
 }
 
 function calc() {
-	if (box.checked && !(box2.checked)) {
-		alert(table.rows[0].cells[0].innerHTML + table.rows[0].cells[2].innerHTML);
-	} else if (box2.checked && !box.checked) {
-		alert(table.rows[0].cells[0].innerHTML * table.rows[0].cells[2].innerHTML);
+	if (box.checked) {
+		alert(0+1);
 	} else {
-		alert('Choose ONLY an operation');
+		alert(0-1);
 	}
 
-function column(args) {
-	for (var i = 0; i < table.rows.length; i++) {
-		table.rows[i].cells[0].innerHTML = args[i];
-	}
-}
+//function column(args) {
+//	for (var i = 0; i < table.rows.length; i++) {
+//		table.rows[i].cells[0].innerHTML = args[i];
+//	}
+//}
 
-function column2(args) {
-	for (var i = 0; i < table.rows.length; i++) {
-		table.rows[i].cells[1].innerHTML = args[i];
-	}
-}
+//function column2(args) {
+//	for (var i = 0; i < table.rows.length; i++) {
+//		table.rows[i].cells[1].innerHTML = args[i];
+//	}
+//}
 
 function setsym(x) {
 	table.rows[0].cells[1].innerHTML = x;
